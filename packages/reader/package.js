f2a22032d230568229825597c99acd282c12cb7a
Daniel Beilinson <beilinson@gmail.com>
2017-03-22 02:30:21 +0300

    Fix reader.

--- packages/reader/package.js
+++ packages/reader/package.js
@@ -1,13 +1,12 @@
-import { BasePackage } from 'substance'
 import { BracketsPackage, TabbedContextPackage } from 'archivist'
 import ReaderLayout from './ReaderLayout'
 
 export default {
   name: 'archivist-reader',
   configure: function(config) {
-    config.import(BasePackage)
     config.import(BracketsPackage)
     config.import(TabbedContextPackage)
     config.addComponent('reader', ReaderLayout)
+    config.addToolGroup('references')
   }
 }
