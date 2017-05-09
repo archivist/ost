01e3c64b1524f7233024f9535391763ef12ffd58
Daniel Beilinson <beilinson@gmail.com>
2017-05-10 01:50:07 +0300

    Handle edit subject call.

--- packages/ost-publisher/OstPublisherContext.js
+++ packages/ost-publisher/OstPublisherContext.js
@@ -3,6 +3,20 @@
 
 import { PublisherContext } from 'archivist'
 
-class OstPublisherContext extends PublisherContext {}
+class OstPublisherContext extends PublisherContext {
+  
+  editSubject(node) {
+    let mode = 'edit'
+    let context = node ? this.contextMap[node.type] : 'subjects'
+    let state = {
+      contextId: context,
+      mode: mode,
+      item: node ? node.id : undefined
+    }
+    this.extendState(state)
+    console.log('Edit container resource', node.id, ',', mode, 'mode')
+  }
+
+}
 
 export default OstPublisherContext
