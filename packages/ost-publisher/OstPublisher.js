7776efea6c6f80b9776ac464f89c8fb39739428c
Daniel Beilinson <beilinson@gmail.com>
2017-05-28 12:55:39 +0300

    General publisher improvements.

--- packages/ost-publisher/OstPublisher.js
+++ packages/ost-publisher/OstPublisher.js
@@ -1,9 +1,17 @@
 // import { ContainerEditor, Highlights, Layout, ProseEditor, SplitPane, Toolbar } from 'substance'
-import { forEach } from 'lodash-es'
+import { forEach, map } from 'lodash-es'
 import OstPublisherContext from './OstPublisherContext'
 import { Publisher } from 'archivist'
 
 class OstPublisher extends Publisher {
+  constructor(...args) {
+    super(...args)
+
+    this.handleActions({
+      'showTopics': this._showTopics
+    })
+  }
+
   didMount() {
     super.didMount()
     let editorSession = this.getEditorSession()
@@ -16,7 +24,9 @@ class OstPublisher extends Publisher {
   }
 
   _onSessionUpdate(editorSession) {
-    if (!editorSession.hasChanged('document') && !editorSession.hasChanged('selection')) return
+    let selectionChanged = editorSession.hasChanged('selection')
+
+    if (!editorSession.hasChanged('document') && !selectionChanged) return
 
     let change = editorSession.getChange()
     if(change) {
@@ -27,68 +37,93 @@ class OstPublisher extends Publisher {
           let node = change.created[id]
           let reference = node.reference
 
-          let resources = editorSession.resources
-          let entity = find(resources, item => { return item.entityId === reference })
-          if(!entity) {
-            let resourceClient = this.context.resourceClient
-            resourceClient.getEntity(reference, (err, entity) => {
-              if (err) {
-                console.error(err)
-              } else {
-                resources.push(entity)
-              }
-            })
+          if(reference) {
+            let resources = editorSession.resources
+            let entity = find(resources, item => { return item.entityId === reference })
+            if(!entity) {
+              let resourceClient = this.context.resourceClient
+              resourceClient.getEntity(reference, (err, entity) => {
+                if (err) {
+                  console.error(err)
+                } else {
+                  resources.push(entity)
+                }
+              })
+            }
           }
         })
       }
 
       // Rerender brackets if subjects were affected
       change.ops.forEach(op => {
-        if(op.path[0].indexOf('subject') >= -1) {
+        if(op.path[0].indexOf('subject') > -1) {
           this.refs.brackets.rerender()
           setTimeout(function() {
             this.refs.brackets.updateBrackets()
           }.bind(this))
         }
       })
+    }
 
-    }    
+    // TODO: figure out why selection flags changed after comment update 
+    if(selectionChanged && !this._isCommentChange(change)) {
 
-    let doc = editorSession.getDocument()
-    let contextPanel = this.refs.contextPanel
+      let doc = editorSession.getDocument()
+      let contextPanel = this.refs.contextPanel
 
-    //let entityIndex = doc.getIndex('entities')
-    let schema = doc.getSchema()
-    let nodes = schema.nodeRegistry.entries
-    let highlights = {}
-    forEach(nodes, node => {
-      if(node.prototype.isResourceReference) {
-        highlights[node.type] = []
-      }
-    })
+      //let entityIndex = doc.getIndex('entities')
+      let schema = doc.getSchema()
+      let nodes = schema.nodeRegistry.entries
+      let highlights = {}
+      forEach(nodes, node => {
+        if(node.prototype.isResourceReference) {
+          highlights[node.type] = []
+        }
+      })
 
-    let overlapsAnno = false
+      let overlapsAnno = false
 
-    let selectionState = editorSession.getSelectionState()
-    forEach(highlights, (h, annoType) => {
-      let annos = selectionState.getAnnotationsForType(annoType)
-      // This will work if inline annotations can't overlap each other
-      // we should check for one node
-      if(annos.length === 1) {
-        let node = annos[0]
-        highlights[annoType] = [node.id]
-        if(node.reference) {
-          contextPanel.openResource(node)
-          overlapsAnno = true
+      let selectionState = editorSession.getSelectionState()
+      forEach(highlights, (h, annoType) => {
+        let annos = selectionState.getAnnotationsForType(annoType)
+        // This will work if inline annotations can't overlap each other
+        // we should check for one node
+        if(annos.length === 1) {
+          let node = annos[0]
+          highlights[annoType] = [node.id]
+          if(node.reference) {
+            let isCollapsed = selectionState.selection.isCollapsed()
+            if(isCollapsed) contextPanel.openResource(node)
+            overlapsAnno = true
+          }
+        }
+      })
+
+      if(!overlapsAnno) {
+        let overlapedComments = selectionState.getAnnotationsForType('comment')
+        if(overlapedComments.length > 0) {
+          contextPanel.openComment(overlapedComments[0])
+        } else {
+          contextPanel.openDefaultTab()
         }
       }
 
-      if(!overlapsAnno) {
-        contextPanel.openDefaultTab()
-      }
-    })
+      this.contentHighlights.set(highlights)
 
-    this.contentHighlights.set(highlights)
+    }
+  }
+
+  _isCommentChange(change) {
+    if(change) {
+      if(change.ops.length === 1) {
+        let path = change.ops[0].path[0]
+        if(path.indexOf('comment') > -1) {
+          return true
+        }
+      }
+    }
+
+    return false
   }
 
   _renderContextSection($$) {
@@ -102,6 +137,25 @@ class OstPublisher extends Publisher {
   _createSubjectReference(anno) {
     let contextPanel = this.refs.contextPanel
     contextPanel.editSubject(anno)
+  }
+
+  _showTopics(topics) {
+    let editorSession = this.editorSession
+    let doc = editorSession.getDocument()
+    let entityIndex = doc.getIndex('entities')
+    let paragraphs = []
+    forEach(topics, topic => {
+      let refs = entityIndex.get(topic)
+      let paras = map(refs, n => {return n.start.path[0]})
+      paragraphs = paragraphs.concat(paras)
+    })
+    let firstPara = doc.getFirst(paragraphs)
+    this.refs.contentPanel.scrollTo(firstPara)
+
+    setTimeout(function(){
+      this.refs.brackets.highlight(topics)
+      this.highlightReferences(topics, true)
+    }.bind(this), 10)
   }
 }
 
