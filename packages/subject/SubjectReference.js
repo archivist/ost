a6f435f2b14a31aee9e33b406e2002374299dad5
Daniel Beilinson <beilinson@gmail.com>
2017-06-07 01:55:20 +0300

    Check for bracket and multiple reference.

--- packages/subject/SubjectReference.js
+++ packages/subject/SubjectReference.js
@@ -5,13 +5,29 @@ import { ContainerAnnotation } from 'substance'
   Used for highlighting subject references inside documents.
 */
 
-class SubjectReference extends ContainerAnnotation {}
+class SubjectReference extends ContainerAnnotation {
+  /**
+    If this annotation is a Bracket Reference.
+    Bracket References will be rendered as bracket right to the content.
+    @returns {Boolean}
+  */
+  isBracketReference() {
+    return true
+  }
+
+  /**
+    If this annotation is a Resource Multiple Reference.
+    Resource Multiple References are annotations with an array reference property.
+    @returns {Boolean}
+  */
+  isResourceMultipleReference() {
+    return true
+  }
+}
 
 SubjectReference.define({
   type: 'subject',
   reference: {type: ["string"], default: []}
 })
-
-SubjectReference.prototype.isBracketReference = true
 
 export default SubjectReference
