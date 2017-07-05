da67e4e5ceb484e7e12acea42fae1fec02d63907
Daniel Beilinson <beilinson@gmail.com>
2017-07-05 19:27:14 +0300

    Introduce definition type filter.

--- packages/definition-manager/DefinitionsPage.js
+++ packages/definition-manager/DefinitionsPage.js
@@ -1,6 +1,62 @@
+import { FontAwesomeIcon as Icon, Input } from 'substance'
 import { AbstractEntityPage } from 'archivist'
 
-class DefinitionsPage extends AbstractEntityPage {}
+const definitionTypes = [
+  'общий комментарий',
+  'лагерная реалия',
+  'сокращение',
+  'языковой комментарий'
+]
+
+class DefinitionsPage extends AbstractEntityPage {
+  renderFilters($$) {
+    let filters = []
+    let search = $$('div').addClass('se-search').append(
+      $$(Icon, {icon: 'fa-search'})
+    )
+    let searchInput = $$(Input, {type: 'search', placeholder: 'Search...'})
+      .ref('searchInput')
+
+    if(this.isSearchEventSupported) {
+      searchInput.on('search', this._onSearch)
+    } else {
+      searchInput.on('keypress', this._onSearchKeyPress)
+    }
+    search.append(searchInput)
+
+    filters.push(search)
+
+    let typeFilter = $$('select').addClass('se-type-filter')
+      .ref('typeFilter')
+      .on('change', this._onSearch)
+      .append($$('option').attr('value', 'all').append('select type'))
+
+    definitionTypes.forEach(type => {
+      let option = $$('option').attr('value', type).append(type)
+      let definitionType = this.state.dataFilters.definitionType
+      if(type === definitionType) option.attr('selected', 'selected')
+      typeFilter.append(option)
+    })
+
+    filters.push($$('div').addClass('se-select').append(typeFilter))
+
+    return filters
+  }
+
+  _onSearch() {
+    let searchValue = this.refs['searchInput'].val()
+    let definitionTypeValue = this.refs['typeFilter'].val()
+    let dataFilters = {}
+    dataFilters['definitionType'] = definitionTypeValue
+    if(definitionTypeValue === 'all') delete dataFilters['definitionType']
+    this.extendState({
+      dataFilters: dataFilters,
+      search: searchValue,
+      pagination: false
+    })
+  }
+
+}
 
 DefinitionsPage.entityType = 'definition'
 DefinitionsPage.pageName = 'definitions'
