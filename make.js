6e511af33ad01c3f36bd8e33d650d20f94574fb2
Daniel Beilinson <beilinson@gmail.com>
2017-08-03 00:27:27 +0300

    Improve make script.

--- make.js
+++ make.js
@@ -1,67 +1,74 @@
-var b = require('substance-bundler');
-var fs = require('fs')
-var config = require('config')
+const b = require('substance-bundler')
+const fs = require('fs')
+const config = require('config')
 
 b.task('clean', function() {
   b.rm('./dist')
 })
-
 // copy assets
 b.task('assets', function() {
-  b.copy('node_modules/font-awesome', './dist/font-awesome')
-  b.copy('node_modules/leaflet.markercluster/dist', './dist/markercluster')
+  b.copy('node_modules/font-awesome', './dist/libs/font-awesome')
+  b.copy('node_modules/leaflet.markercluster/dist', './dist/libs/markercluster')
   b.copy('node_modules/leaflet/dist/images', './dist/publisher/images')
   b.copy('node_modules/leaflet-control-geocoder/dist/images', './dist/publisher/images')
 })
-
-// this optional task makes it easier to work on Substance core
-b.task('substance', function() {
-  b.make('substance', 'build')
-  b.copy('node_modules/substance/dist', './dist/substance')
-  b.minify('./dist/substance/substance.js', './dist/substance/substance.min.js')
+// dev
+b.task('publisher', buildApp('publisher'))
+b.task('scholar', buildApp('scholar'))
+// production
+b.task('publisher-min', buildApp('publisher', true))
+b.task('scholar-min', buildApp('scholar', true))
+// build apps
+b.task('client', ['publisher', 'scholar'])
+b.task('client-min', ['scholar-min', 'publisher-min'])
+// build libraries
+b.task('deps', () => {
+  _buildDeps()
+})
+b.task('deps-min', () => {
+  _buildDeps(true)
+})
+// build server js
+b.task('server', () => {
+  buildServerJS()
 })
 
-b.task('archivist', function() {
-  b.make('archivist', 'build')
-  b.copy('node_modules/archivist/dist', './dist/archivist')
-  b.minify('./dist/archivist/archivist.js', './dist/archivist/archivist.min.js')
-})
-
-function buildArchivistDev() {
-  return function() {
-    b.make('archivist', 'dev')
-    b.copy('node_modules/archivist/dist', './dist/archivist')
-  }
-}
+// build all
+b.task('default', ['dev'])
+b.task('dev', ['clean', 'assets', 'deps', 'server', 'client'])
+b.task('production', ['clean', 'assets', 'deps-min', 'server', 'client-min'])
 
 function buildApp(app, production) {
   return function() {
     if(production) {
-      b.copy('client/'+app+'/index.production.html', './dist/'+app+'/index.html')
+      b.copy('client/'+ app +'/index.production.html', './dist/'+ app +'/index.html')
     } else {
-      b.copy('client/'+app+'/index.html', './dist/'+app+'/')
+      b.copy('client/'+ app +'/index.html', './dist/'+ app +'/')
     }
-    b.copy('client/'+app+'/assets', './dist/'+app+'/assets/')
-    b.css('./client/' + app + '/app.css', 'dist/' + app + '/' + app + '.css', {variables: true})
+    b.copy('client/'+ app +'/assets', './dist/'+ app +'/assets/')
+    b.css('client/' + app + '/app.css', 'dist/' + app + '/' + app + '.css')
+
     b.js('client/' + app + '/app.js', {
-      // need buble if we want to minify later
-      buble: true,
-      external: ['substance', 'archivist'],
-      commonjs: { 
+      targets: [{
+        dest: './dist/' + app + '/app.js',
+        format: 'umd', moduleName: 'app'
+      }],
+      commonjs: {
         include: [
           'node_modules/moment/moment.js', 
           'node_modules/plyr/src/js/plyr.js', 
           'node_modules/leaflet/dist/leaflet-src.js',
           'node_modules/leaflet.markercluster/dist/leaflet.markercluster-src.js',
           'node_modules/leaflet-control-geocoder/dist/Control.Geocoder.js'
-        ] 
+        ]
       },
-      targets: [{
-        dest: './dist/' + app + '/app.js',
-        format: 'umd',
-        moduleName: app
-      }]
+      external: ['substance', 'archivist'],
+      globals: {
+        'substance': 'substance',
+        'archivist': 'archivist'
+      }
     })
+    
     b.custom('injecting config', {
       src: './dist/' + app + '/app.js',
       dest: './dist/' + app + '/' + app + '.js',
@@ -71,47 +78,38 @@ function buildApp(app, production) {
         fs.writeFileSync(this.outputs[0], result, 'utf8')
       }      
     })
-    b.minify('./dist/' + app + '/' + app + '.js', './dist/' + app + '/' + app + '.min.js')
-    b.copy('./dist/' + app + '/app.js.map', './dist/' + app + '/' + app + '.js.map')
+    if(production) {
+      b.minify('./dist/' + app + '/' + app + '.js', './dist/' + app + '/' + app + '.min.js')
+    } else {
+      b.copy('./dist/' + app + '/app.js.map', './dist/' + app + '/' + app + '.js.map')
+    }
     b.rm('./dist/' + app + '/app.js')
     b.rm('./dist/' + app + '/app.js.map')
   }
 }
 
-function _ostJS() {
+function buildServerJS() {
   b.js('./index.es.js', {
-    buble: true,
     external: ['substance', 'archivist'],
+    globals: {
+      'substance': 'substance',
+      'archivist': 'archivist'
+    },
     targets: [{
       dest: 'dist/ost.cjs.js',
-      format: 'cjs', 
-      sourceMapRoot: __dirname, 
-      sourceMapPrefix: 'ost'
+      format: 'cjs'
     }]
   })
 }
 
-b.task('deps', ['substance', 'assets', 'archivist'])
-b.task('ost', _ostJS())
+/* HELPERS */
 
-// dev
-b.task('archivist-dev', buildArchivistDev())
-b.task('publisher', buildApp('publisher'))
-b.task('scholar', buildApp('scholar'))
-// production
-b.task('publisher-min', buildApp('publisher', true))
-b.task('scholar-min', buildApp('scholar', true))
+function _buildDeps(min) {
+  b.make('substance', 'lib')
+  b.copy('node_modules/substance/dist', './dist/libs/substance')
+  if(min) b.minify('./dist/libs/substance/substance.js', './dist/libs/substance/substance.min.js')
 
-b.task('client', ['publisher', 'scholar'])
-b.task('client-min', ['scholar-min', 'publisher-min'])
-
-// build all
-b.task('default', ['deps', 'client', 'ost'])
-b.task('dev', ['substance', 'assets', 'archivist-dev', 'client', 'ost'])
-b.task('production', ['deps', 'client-min', 'ost'])
-
-// starts a server when CLI argument '-s' is set
-b.setServerPort(5001)
-b.serve({
-  static: true, route: '/', folder: 'dist'
-});
+  b.make('archivist', 'lib')
+  b.copy('node_modules/archivist/dist', './dist/libs/archivist')
+  if(min) b.minify('./dist/libs/archivist/archivist.js', './dist/libs/archivist/archivist.min.js')
+}
